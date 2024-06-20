ff81cc6e6cb2486c8be050568c0dc93e035e9193
ytchka <103581838+ytchka@users.noreply.github.com>
2024-06-20 10:59:12 +0300

    Add files via upload

---
+++ build/CMakeFiles/MyProject.dir/A.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/MyProject.dir/A.cpp.o: /home/ytchka/t1/A.cpp \
+ /usr/include/stdc-predef.h /home/ytchka/t1/A.h
---
+++ build/CMakeFiles/MyProject.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/MyProject.dir/main.cpp.o: /home/ytchka/t1/main.cpp \
+ /usr/include/stdc-predef.h /home/ytchka/t1/class.h
